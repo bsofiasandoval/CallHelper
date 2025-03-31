a22f79fbc44374d31e685a7b429b71a6fa9e44d0
Brenda Sofia Sandoval <bsofiasandoval2@gmail.com>
2025-03-31 11:51:05 -0600

    delete unused folder

--- web/src/lib/auth.ts
+++
@@ -1,7 +0,0 @@
-import { betterAuth } from "better-auth";
-import { nextCookies } from "better-auth/next-js";
-import { organization } from "better-auth/plugins";
-
-export const auth = betterAuth({
-  plugins: [organization(), nextCookies()], // nextCookies needs to be the last plugin in the array
-});
